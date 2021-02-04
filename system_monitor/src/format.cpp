ab53b7c3758ce47ce83b47ccf5b3e473a6ec3a39
Uira Caiado <ucaiado@msn.com>
2021-02-04 01:15:29 -0300

    feat: implement ElapsedTime formatting function #2

--- system_monitor/src/format.cpp
+++ system_monitor/src/format.cpp
@@ -1,5 +1,5 @@
 #include <string>
-
+#include <cmath>
 #include "format.h"
 
 using std::string;
@@ -7,5 +7,14 @@ using std::string;
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    int hours_ = seconds/pow(60, 2);
+    int minutes_ = (seconds - hours_ * pow(60, 2))/60;
+    int seconds_ = (seconds - hours_ * pow(60, 2) - minutes_ * 60);
+    //string str_output;
+    char str_output[25];
+    sprintf(str_output, "%02d:%02d:%02d", hours_, minutes_, seconds_);
+
+    // return std::to_string(seconds);
+    return str_output;
+}
