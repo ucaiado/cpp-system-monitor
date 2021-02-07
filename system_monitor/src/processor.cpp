1b6daa8746d9ff95a1a12f53ac1f8793e8bd60d9
Uira Caiado <ucaiado@msn.com>
2021-02-07 02:03:33 -0300

    feat: implement agg cpu utilization calculation #9

--- system_monitor/src/processor.cpp
+++ system_monitor/src/processor.cpp
@@ -1,4 +1,17 @@
 #include "processor.h"
+#include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+// Return the aggregate CPU utilization
+float Processor::Utilization() {
+  // source: https://bit.ly/3cPFsrg
+  // NonIdle = user + nice + system + irq + softirq + steal
+  float f_nonidle = LinuxParser::ActiveJiffies();
+  // Idle = idle + iowait
+  float f_idle = LinuxParser::IdleJiffies();
+  // Total = Idle + NonIdle
+  // CPU_Percentage = (Total - idled)/totald
+  if ((f_nonidle + f_idle) == 0) {
+    return 0.0;
+  }
+  return (f_nonidle / (f_nonidle + f_idle));
+}
