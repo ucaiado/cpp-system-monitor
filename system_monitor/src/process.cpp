01e75d9b079f090d3010a30c8a5687232cb258e6
Uira Caiado <ucaiado@msn.com>
2021-02-08 00:57:39 -0300

    feat: implement functions related to process cpp #7

--- system_monitor/src/process.cpp
+++ system_monitor/src/process.cpp
@@ -10,24 +10,33 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+// Return this process's ID
+int Process::Pid() { return pid_; }
+void Process::Pid(int pid) { pid_ = pid; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+// Return this process's CPU utilization
+float Process::CpuUtilization() { return process_cpu_; }
+void Process::CpuUtilization(float process_cpu) { process_cpu_ = process_cpu; }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// Return the command that generated this process
+string Process::Command() { return cmd_; }
+void Process::Command(string cmd) { cmd_ = cmd; }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// Return this process's memory utilization
+string Process::Ram() { return process_ram_; }
+void Process::Ram(string process_ram) { process_ram_ = process_ram; }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// Return the user (name) that generated this process
+string Process::User() { return user_; }
+void Process::User(string user) { user_ = user; }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// Return the age of this process (in seconds)
+long int Process::UpTime() { return process_uptime_; }
+void Process::UpTime(long int process_uptime) {
+  process_uptime_ = process_uptime;
+}
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+// Overload the "less than" comparison operator for Process objects
+bool Process::operator<(Process const& a) const {
+  return a.CpuUtilization() < process_cpu_;
+}
