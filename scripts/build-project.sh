8de96b2e31c9a0c82cf9459f57ee0fee538b59e3
Uira Caiado <ucaiado@msn.com>
2021-02-10 01:53:00 -0300

    refactor: include some random process to be monitored

--- scripts/build-project.sh
+++ scripts/build-project.sh
@@ -4,7 +4,12 @@ BuildDir="build"
 cd /CppND-System-Monitor-Project-Updated/
 
 # build project
+make clean
 make build
+
+# start some process to monitor
+for i in {1..3}; do echo -n ""; date ; sleep 5; done > /dev/null 2>&1 &
+watch free -m > /dev/null 3>&1 &
 
 # run monitor
 ./build/monitor
